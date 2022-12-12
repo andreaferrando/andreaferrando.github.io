cb1d5db8c910cdb4a07adccf3e450fb7606638e0
andreaferrando <andrea.ferrando@icloud.com>
2022-12-12 15:44:51 +0000

    nil

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ const App = () => {
               </>
             }
           />
-          <Route path="*" element={ <Navigate to="/" /> } />
+          {/* <Route path="*" element={ <Navigate to="/" /> } /> */}
         </Routes>
       </BrowserRouter>
       <Footer />
